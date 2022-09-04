74e6ee7880dadeaef52849cbdfebe9a3d7c126e4
CPSOe <8696301+CPSOe@user.noreply.gitee.com>
2022-09-04 19:48:33 +0800

    V1.1-1.0
    更改main_test.ino
    解决触摸无法中断的问题
    硬件全部验证完毕

--- build/README.md
+++ build/README.md
@@ -1,8 +1,8 @@
 # OeScreen V1.1-1.0
-\bulid
+/bulid
 此文件夹包含编译所需文件：
-    .\data          -包含需存储至Falsh文件系统的文件
-    .\main.ino      -Arduino程序文件
+    ./data          -包含需存储至Falsh文件系统的文件
+    ./main.ino      -Arduino程序文件
 编译说明:
     1.上传程序前请先添加esp8266fs插件，
         在工具-Flash Size选择“4MB(FS：1MB ...)”并上传文件(参考http://www.taichi-maker.com/homepage/esp8266-nodemcu-iot/iot-c/spiffs/upload-files/)
--- tests/main_test/main_test.ino
+++ tests/main_test/main_test.ino
@@ -21,159 +21,146 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "cn.pool.ntp.org", 60 * 60 * 8, 100);
 TFT_eSPI tft = TFT_eSPI();
 
-#define SHT30_Adr 0x44 //下拉为0x44，上拉为0x45
-#define SDA_Pin 4
-#define SCL_Pin 5
-#define Touch_Pin 12 //触摸芯片输出引脚
-#define Beep_Pin 16  //蜂鸣器引脚
+#define SHT30Adr 0x44 //下拉为0x44，上拉为0x45
+#define SDAPin 4      // SDA引脚(硬件)
+#define SCLPin 5      // SCL引脚(硬件)
+#define touchPin 12   //触摸芯片输出引脚
+#define beepPin 16    //蜂鸣器引脚
 
 const char *ssid = "<SSID>";             // wifi名称
 const char *password = "<PASSWORD>";     // wifi密码
-float SHT30_Data[2] = {0, 0};            // 摄氏度,湿度
+float SHT30Data[2] = {0, 0};             // 摄氏度,湿度
 int nameBlue = tft.color565(48, 64, 82); // CPSOe主题颜色
 
-void PinInterrupt();
-int SHT30_getData();
+ICACHE_RAM_ATTR void TouchInterrupt(); //触摸中断函数
+bool SHT30GetData();                   //获取SHT30的数据   传入温度及湿度的float类型数组指针，元素0为温度，元素1为湿度
 
 void setup()
 {
-  Serial.begin(115200);
-  Wire.begin(SDA_Pin, SCL_Pin);
-  pinMode(Touch_Pin, INPUT);
-  attachInterrupt(Touch_Pin, PinInterrupt, RISING); //设置中断函数
+    //通讯初始化
+    Serial.begin(115200);
+    Wire.begin(SDAPin, SCLPin);
 
-  // tft屏幕初始化
-  tft.init();
-  tft.fillScreen(TFT_BLACK);
-  tft.setRotation(0);
-  tft.setCursor(0, 0, 1);
+    // tft屏幕初始化
+    tft.init();
+    pinMode(touchPin, INPUT);
+    tft.fillScreen(TFT_BLACK);
+    tft.setRotation(2);
+    tft.setCursor(0, 0, 1);
 
-  // tft屏幕显示项目名，作者名
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(2);
-  tft.drawString("OeScreen", 5, 5);
-  tft.setTextColor(nameBlue);
-  tft.drawString("LiKongDian", 5, 23);
-  tft.drawString("CPSOe", 5, 38);
+    //触摸初始化 注意:触摸引脚初始化放在tft.init()后,否则触摸引脚被占用，无法读取及中断
+    pinMode(touchPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(touchPin), TouchInterrupt, RISING); //设置中断
 
-  //连接wifi
-  WiFi.begin(ssid, password);
+    // tft屏幕显示项目名，作者名
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(2);
+    tft.drawString("OeScreen", 5, 5);
+    tft.setTextColor(nameBlue);
+    tft.drawString("LiKongDian", 5, 23);
+    tft.drawString("CPSOe", 5, 38);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
+    //连接wifi
+    WiFi.begin(ssid, password);
     Serial.print("connecting");
-    delay(500);
-    Serial.print(".");
-  }
-  timeClient.begin();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    timeClient.begin();
 }
 
 void loop()
 {
-  //获取温度，湿度
-  float *SHT30_Data_p = SHT30_Data;
-  SHT30_getData(SHT30_Data_p);
+    //获取温度，湿度
+    float *SHT30Data_p = SHT30Data;
+    SHT30GetData(SHT30Data_p);
 
-  //获取时间
-  timeClient.update();
-  unsigned long epochTime = timeClient.getEpochTime();
-  struct tm *ptm = gmtime((time_t *)&epochTime);
+    //获取时间
+    timeClient.update();
+    unsigned long epochTime = timeClient.getEpochTime();
+    struct tm *ptm = gmtime((time_t *)&epochTime);
 
-  //串口输出数据
-  Serial.print(ptm->tm_year + 1900); //年
-  serial.print("/");
-  Serial.print(ptm->tm_mon + 1); //月
-  serial.print("/");
-  Serial.println(ptm->tm_mday + 1); //日
+    //串口输出数据
+    Serial.println(digitalRead(touchPin));
 
-  Serial.print("星期");
-  Serial.println(ptm->tm_wday + 1); //星期
+    Serial.print(ptm->tm_hour); //时
+    Serial.print(":");
+    Serial.print(ptm->tm_min + 1); //分
+    Serial.print(":");
+    Serial.println(ptm->tm_sec); //秒
 
-  Serial.print(ptm->tm_hour); //时
-  Serial.print(":");
-  Serial.print(ptm->tm_min); //分
-  Serial.print(":");
-  Serial.println(ptm->tm_sec); //秒
+    Serial.print(SHT30Data[0]); //温度
+    Serial.println("℃");
+    Serial.print(SHT30Data[1]); //湿度
+    Serial.println("%RH");
 
-  Serial.print(SHT30_Data[0]); //温度
-  Serial.println("℃");
-  Serial.print(SHT30_Data[1]); //湿度
-  Serial.println("%RH");
+    //将浮点型数据转换为字符串
+    char SHT30DegC[7];
+    char SHT30Humidity[7];
+    dtostrf(SHT30Data[0], 3, 2, SHT30DegC);
+    dtostrf(SHT30Data[1], 3, 2, SHT30Humidity);
 
-  //将浮点型数据转换为字符串
-  char degTemp[7];
-  char humidity[7];
-  dtostrf(SHT30_Data[0], 3, 2, degTemp);
-  dtostrf(SHT30_Data[1], 3, 2, humidity);
+    //刷新部分屏幕
+    tft.fillRect(5, 57, 70, 25, TFT_BLACK);
 
-  // tft屏幕显示温度
-  tft.setTextColor(TFT_RED);
-  tft.setTextSize(1);
-  tft.drawString(degTemp, 5, 55);
-  tft.drawString("DegC", 45, 55);
+    // tft屏幕显示温度
+    tft.setTextColor(TFT_RED);
+    tft.setTextSize(1);
+    tft.drawString(SHT30DegC, 5, 57);
+    tft.drawString("DegC", 41, 57);
 
-  // tft屏幕显示湿度
-  tft.setTextColor(TFT_YELLOW);
-  tft.setTextSize(1);
-  tft.drawString(humidity, 5, 70);
-  tft.drawString("%RH", 45, 70);
+    // tft屏幕显示湿度
+    tft.setTextColor(TFT_YELLOW);
+    tft.setTextSize(1);
+    tft.drawString(SHT30Humidity, 5, 72);
+    tft.drawString("%RH", 41, 72);
 
-  delay(300);
-
-  //刷新屏幕
-  tft.setTextColor(TFT_BLACK);
-  tft.setTextSize(1);
-  tft.drawString(degTemp, 5, 55);
-  tft.drawString(humidity, 5, 70);
-}
-
-ICACHE_RAM_ATTR void PinInterrupt() //触摸中断
-{
-  if (digitalRead(Touch_Pin))
-  {
-    Serial.println("TouchInterrupt");
-    tft.drawFastVLine(40, 88, 128, nameBlue);
-    analogWrite(Beep_Pin, 513);
-    analogWriteFreq(440);
     delay(100);
-    tft.drawFastVLine(40, 88, 128, TFT_BLACK);
-    analogWrite(Beep_Pin, 0);
-  }
 }
 
-int SHT30_getData(float *SHT30_Output) //传入温度及湿度的float类型数组指针，元素0为温度，元素1为湿度
+ICACHE_RAM_ATTR void TouchInterrupt() //触摸中断
 {
-  unsigned int SHT30_Request[6];
-  Wire.beginTransmission(SHT30_Adr);
+    analogWrite(beepPin, 512);
+    analogWriteFreq(698);
+    delayMicroseconds(200000);
+    analogWrite(beepPin, 0);
+}
 
-  Wire.write(0x2C);
-  Wire.write(0x06);
+bool SHT30GetData(float *SHT30Output) //传入温度及湿度的float类型数组指针，元素0为温度，元素1为湿度
+{
+    unsigned int RequestData[6];
+    Wire.beginTransmission(SHT30Adr);
 
-  Wire.endTransmission();
+    Wire.write(0x2C);
+    Wire.write(0x06);
 
-  delay(15);
+    Wire.endTransmission();
 
-  Wire.requestFrom(SHT30_Adr, 6);
+    delay(15);
 
-  //读取6字节的数据
-  //这六个字节分别为：温度8位高数据，温度8位低数据，温度8位CRC校验数据
-  //               湿度8位高数据，湿度8位低数据，湿度8位CRC校验数据
-  if (Wire.available() == 6)
-  {
-    SHT30_Request[0] = Wire.read();
-    SHT30_Request[1] = Wire.read();
-    SHT30_Request[2] = Wire.read();
-    SHT30_Request[3] = Wire.read();
-    SHT30_Request[4] = Wire.read();
-    SHT30_Request[5] = Wire.read();
-  }
-  else
-  {
-    return 0;
-  }
-  float r_degTemp = ((((SHT30_Request[0] * 256.0) + SHT30_Request[1]) * 175) / 65535.0) - 45;
-  float r_humidity = ((((SHT30_Request[3] * 256.0) + SHT30_Request[4]) * 100) / 65535.0);
-  *SHT30_Output = r_degTemp;
-  *(SHT30_Output + 1) = r_humidity;
-  return 1;
+    Wire.requestFrom(SHT30Adr, 6);
+
+    //读取6字节的数据
+    //这六个字节分别为：温度8位高数据，温度8位低数据，温度8位CRC校验数据
+    //               湿度8位高数据，湿度8位低数据，湿度8位CRC校验数据
+    if (Wire.available() == 6)
+    {
+        RequestData[0] = Wire.read();
+        RequestData[1] = Wire.read();
+        RequestData[2] = Wire.read();
+        RequestData[3] = Wire.read();
+        RequestData[4] = Wire.read();
+        RequestData[5] = Wire.read();
+    }
+    else
+    {
+        return 0;
+    }
+    float RequestDeg = ((((RequestData[0] * 256.0) + RequestData[1]) * 175) / 65535.0) - 45;
+    float RequestHumidity = ((((RequestData[3] * 256.0) + RequestData[4]) * 100) / 65535.0);
+    *SHT30Output = RequestDeg;
+    *(SHT30Output + 1) = RequestHumidity;
+    return 1;
 }
