8e594df0b748027d0937a12012d62b6bf3be3f2b
CPSOe <8696301+CPSOe@user.noreply.gitee.com>
2022-09-12 10:17:53 +0800

    V1.1-1.0 更改综合测试程序 1.添加、更改说明 2.更换ntp服务器地址为中国教育网time.edu.cn 3.统一背景颜色定义，方便更换背景颜色 4.更改变量名nameBlue为CPSOeColor 5.添加wifi连接屏幕提示 6.添加屏幕时间显示 7.添加触摸中断提示动画

--- tests/main_test/main_test.ino
+++ tests/main_test/main_test.ino
@@ -1,8 +1,8 @@
 /*
-V1.1
+V1.1-1.0
 作者：理空电CPSOe
 此程序为OeScreen开发板的综合测试程序
-将在屏幕上显示此开发板的信息
+连接wifi后，将在屏幕上显示读取的温湿度及时间
 开源链接：https://gitee.com/CPSOe/OeScreen
 许可证：MIT
 */
@@ -18,7 +18,7 @@ V1.1
 #include <WiFiUdp.h>
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "cn.pool.ntp.org", 60 * 60 * 8, 100);
+NTPClient timeClient(ntpUDP, "time.edu.cn", 60 * 60 * 8, 100); // time.edu.cn为ntp服务器地址，60*60*8设置时区(中国东八时区，北京时间)，最后一个参数设置更新最小间隔
 TFT_eSPI tft = TFT_eSPI();
 
 #define SHT30Adr 0x44 //下拉为0x44，上拉为0x45
@@ -26,14 +26,15 @@ TFT_eSPI tft = TFT_eSPI();
 #define SCLPin 5      // SCL引脚(硬件)
 #define touchPin 12   //触摸芯片输出引脚
 #define beepPin 16    //蜂鸣器引脚
+#define backgroundColor TFT_BLACK
 
-const char *ssid = "<SSID>";             // wifi名称
-const char *password = "<PASSWORD>";     // wifi密码
-float SHT30Data[2] = {0, 0};             // 摄氏度,湿度
-int nameBlue = tft.color565(48, 64, 82); // CPSOe主题颜色
+const char *ssid = "SSID";                 // wifi名称
+const char *password = "PASSWORD";         // wifi密码
+float SHT30Data[2] = {0, 0};               // 摄氏度,湿度浮点型数据
+int CPSOeColor = tft.color565(48, 64, 82); // CPSOe主题颜色 rgb(48,64,82)
 
 ICACHE_RAM_ATTR void TouchInterrupt(); //触摸中断函数
-bool SHT30GetData();                   //获取SHT30的数据   传入温度及湿度的float类型数组指针，元素0为温度，元素1为湿度
+bool SHT30GetData(float *SHT30Output); //获取SHT30的数据   传入温度及湿度的float类型数组指针，元素0为温度，元素1为湿度
 
 void setup()
 {
@@ -44,11 +45,11 @@ void setup()
     // tft屏幕初始化
     tft.init();
     pinMode(touchPin, INPUT);
-    tft.fillScreen(TFT_BLACK);
+    tft.fillScreen(backgroundColor);
     tft.setRotation(2);
     tft.setCursor(0, 0, 1);
 
-    //触摸初始化 注意:触摸引脚初始化放在tft.init()后,否则触摸引脚被占用，无法读取及中断
+    //触摸初始化 注意:由于触摸引脚使用硬件MISO(数据输入，本项目使用的tft屏幕没有MISO引脚),触摸引脚初始化放在tft.init()后,否则触摸引脚无法读取及中断
     pinMode(touchPin, INPUT);
     attachInterrupt(digitalPinToInterrupt(touchPin), TouchInterrupt, RISING); //设置中断
 
@@ -56,18 +57,24 @@ void setup()
     tft.setTextColor(TFT_WHITE);
     tft.setTextSize(2);
     tft.drawString("OeScreen", 5, 5);
-    tft.setTextColor(nameBlue);
+    tft.setTextColor(CPSOeColor);
     tft.drawString("LiKongDian", 5, 23);
     tft.drawString("CPSOe", 5, 38);
 
     //连接wifi
     WiFi.begin(ssid, password);
     Serial.print("connecting");
-    while (WiFi.status() != WL_CONNECTED)
+    tft.drawString("connecting", 5, 65);
+    for (int i = 5; WiFi.status() != WL_CONNECTED; i += 6)
     {
-        delay(500);
+        delay(700);
+        tft.drawString(".", i, 82);
         Serial.print(".");
     }
+    tft.setTextColor(backgroundColor);
+    tft.drawString("connecting", 5, 65);
+    Serial.print("connected\nto\n");
+    Serial.println(ssid);
     timeClient.begin();
 }
 
@@ -82,12 +89,10 @@ void loop()
     unsigned long epochTime = timeClient.getEpochTime();
     struct tm *ptm = gmtime((time_t *)&epochTime);
 
-    //串口输出数据
-    Serial.println(digitalRead(touchPin));
-
+    //串口输出
     Serial.print(ptm->tm_hour); //时
     Serial.print(":");
-    Serial.print(ptm->tm_min + 1); //分
+    Serial.print(ptm->tm_min); //分
     Serial.print(":");
     Serial.println(ptm->tm_sec); //秒
 
@@ -102,8 +107,8 @@ void loop()
     dtostrf(SHT30Data[0], 3, 2, SHT30DegC);
     dtostrf(SHT30Data[1], 3, 2, SHT30Humidity);
 
-    //刷新部分屏幕
-    tft.fillRect(5, 57, 70, 25, TFT_BLACK);
+    //刷新温湿度显示(填充背景颜色)
+    tft.fillRect(5, 57, 70, 25, backgroundColor);
 
     // tft屏幕显示温度
     tft.setTextColor(TFT_RED);
@@ -117,14 +122,71 @@ void loop()
     tft.drawString(SHT30Humidity, 5, 72);
     tft.drawString("%RH", 41, 72);
 
-    delay(100);
+    //刷新时间显示(填充背景颜色)
+    tft.fillRect(5, 90, 82, 16, TFT_BLACK);
+
+    //转换时间数据为字符串，若小于10，则在前面添加“0”
+    char charHours[4];
+    char charMinutes[4];
+    char charSeconds[4];
+    String strHours;
+    String strMinutes;
+    String strSeconds;
+
+    itoa(ptm->tm_hour, charHours, 10);
+    if (ptm->tm_hour < 10)
+    {
+        strHours = '0';
+        strHours += charHours;
+    }
+    else
+    {
+        strHours = charHours;
+    }
+
+    itoa(ptm->tm_min, charMinutes, 10);
+    if (ptm->tm_min < 10)
+    {
+        strMinutes = '0';
+        strMinutes += charMinutes;
+    }
+    else
+    {
+        strMinutes = charMinutes;
+    }
+
+    itoa(ptm->tm_sec, charSeconds, 10);
+    if (ptm->tm_sec < 10)
+    {
+        strSeconds = '0';
+        strSeconds += charSeconds;
+    }
+    else
+    {
+        strSeconds = charSeconds;
+    }
+
+    // tft屏幕显示时间
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(2);
+    tft.drawString(strHours, 5, 90);
+    tft.drawString(":", 26, 90);
+    tft.drawString(strMinutes, 35, 90);
+    tft.drawString(":", 56, 90);
+    tft.drawString(strSeconds, 65, 90);
+
+    delay(300);
 }
 
 ICACHE_RAM_ATTR void TouchInterrupt() //触摸中断
 {
     analogWrite(beepPin, 512);
     analogWriteFreq(698);
-    delayMicroseconds(200000);
+    tft.fillCircle(119, 151, 2, TFT_WHITE);
+    delayMicroseconds(100000);
+    tft.fillCircle(119, 151, 4, TFT_WHITE);
+    delayMicroseconds(110000);
+    tft.fillRect(115, 147, 10, 10, TFT_BLACK);
     analogWrite(beepPin, 0);
 }
 
